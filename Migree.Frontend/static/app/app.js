dd8e388410e2b475063035f2a005f4657ef9f918
Erik Knutsson <erikkentknutsson@gmail.com>
2015-12-05 17:01:47 +0100

    updated profile-view with stack

--- Migree.Frontend/static/app/app.js
+++ Migree.Frontend/static/app/app.js
@@ -92,6 +92,15 @@ app.controller('StartController', function($scope, $http){
 app.controller('DashboardController', function($scope, $http){
 
 
-
+    new ElastiStack( document.getElementById('stack'), {
+      // distDragBack: if the user stops dragging the image in a area that does not exceed [distDragBack]px 
+      // for either x or y then the image goes back to the stack 
+      distDragBack : 50,
+      // distDragMax: if the user drags the image in a area that exceeds [distDragMax]px 
+      // for either x or y then the image moves away from the stack 
+      distDragMax : 150,
+      // callback
+      onUpdateStack : function( current ) { return false; }
+    } );
 
 });
