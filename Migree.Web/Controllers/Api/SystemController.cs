46265e9c2e6780130411ba49d7025f023aa5ede2
Johan Olsson <johan.olsson@dearfriends.se>
2015-12-05 13:54:22 +0100

    Added dictionary items

---
+++ Migree.Web/Controllers/Api/SystemController.cs
@@ -0,0 +1,26 @@
+﻿using Migree.Web.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace Migree.Web.Controllers.Api
+{
+    [RoutePrefix("api/system")]
+    public class SystemController : MigreeApiController
+    {
+        [HttpGet]
+        [Route("language")]
+        public virtual HttpResponseMessage Language()
+        {
+            var dictionary = new LanguageDictionary();
+            dictionary.Add("login", new DictionaryItem { en = "Login", sv = "Logga in" });
+            dictionary.Add("password", new DictionaryItem { en = "Password", sv = "Lösenord" });
+            return CreateApiResponse(HttpStatusCode.OK, dictionary);
+        }
+    }
+}
---
+++ Migree.Web/Models/DictionaryItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migree.Web.Models
+{
+    public class DictionaryItem
+    {
+        public string en { get; set; }
+        public string sv { get; set; }
+    }
+}
