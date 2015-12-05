be42a3c4213318097f278b51698e807972010783
Johan Olsson <johan.olsson@dearfriends.se>
2015-12-05 15:48:31 +0100

    Added competence list api call

--- Migree.Web/Controllers/Api/UserController.cs
+++ Migree.Web/Controllers/Api/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Migree.Core.Interfaces;
 using Migree.Web.Models.Requests;
+using Migree.Web.Models.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -35,7 +36,7 @@ namespace Migree.Web.Controllers.Api
         public HttpResponseMessage Register(RegisterRequest request)
         {
             var user = UserServant.Register(request.Email, request.Password, request.FirstName, request.LastName, request.UserType);
-            return CreateApiResponse(HttpStatusCode.OK, user.Id);
+            return CreateApiResponse(HttpStatusCode.OK, new RegisterResponse { UserId = user.Id });
         }
     }
 }
