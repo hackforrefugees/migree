055f5f3ab30d747b22f122541be07ff94bee70e1
Johan Olsson <johan.olsson@dearfriends.se>
2015-12-05 15:08:18 +0100

    camelCase ftw!

--- Migree.Web/Models/Requests/RegisterRequest.cs
+++ Migree.Web/Models/Requests/RegisterRequest.cs
@@ -5,15 +5,15 @@ namespace Migree.Web.Models.Requests
 {
     public class RegisterRequest
     {
-        [JsonProperty(PropertyName = "firstname")]
+        [JsonProperty(PropertyName = "firstName")]
         public string FirstName { get; set; }
-        [JsonProperty(PropertyName = "lastname")]
+        [JsonProperty(PropertyName = "lastName")]
         public string LastName { get; set; }
         [JsonProperty(PropertyName = "email")]
         public string Email { get; set; }
         [JsonProperty(PropertyName = "password")]
         public string Password { get; set; }
-        [JsonProperty(PropertyName = "usertype")]
+        [JsonProperty(PropertyName = "userType")]
         public UserType UserType { get; set; }
         
     }
