cb720cb07ff2612904ea7737a3224276a390cfae
Johan Olsson <johan.olsson@dearfriends.se>
2015-12-06 15:28:25 +0100

    Added client properties in token + fixed oauth startup problem

--- Migree.Web/static/app/controllers/dashboardController.js
+++ Migree.Web/static/app/controllers/dashboardController.js
@@ -1,10 +1,8 @@
-migree.controller('DashboardController', ['$scope', function($scope) {
-
+migree.controller('DashboardController', ['$scope', 'authService', function ($scope, authService) {
+    
   new ElastiStack( document.getElementById('stack'), {
     distDragBack : 50,
     distDragMax : 150,
     onUpdateStack : function( current ) { return false; }
   });
-
-
 }]);
