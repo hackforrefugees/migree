6cc7f68562f37af00f05a814564e8897e7a0dbdc
Johan Olsson <johan.olsson@dearfriends.se>
2015-12-06 15:45:06 +0100

    had to override TokenEndpoint

--- Migree.Api/Providers/AuthorizationServerProvider.cs
+++ Migree.Api/Providers/AuthorizationServerProvider.cs
@@ -50,5 +50,13 @@ namespace Migree.Api.Providers
                 return;
             }
         }
+        public override Task TokenEndpoint(OAuthTokenEndpointContext context)
+        {
+            foreach (KeyValuePair<string, string> property in context.Properties.Dictionary)
+            {
+                context.AdditionalResponseParameters.Add(property.Key, property.Value);
+            }
+            return Task.FromResult<object>(null);
+        }
     }
 }
