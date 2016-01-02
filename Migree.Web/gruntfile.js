63277b0ea0aca96dab711673b592eb38ce933d3d
Andreas Urech <andreas.urech@gmail.com>
2016-01-02 09:06:32 +0100

    .tmp-flow working

--- Migree.Web/gruntfile.js
+++ Migree.Web/gruntfile.js
@@ -1,151 +1,489 @@
-module.exports = function(grunt) {
-  var modRewrite = require('connect-modrewrite');
-  var serveStatic = require('serve-static');
-  var mountFolder = function (connect, dir) {
-    return serveStatic(require('path').resolve(dir));
-};
-  // Project configuration.
+// Generated on 2015-12-31 using generator-angular 0.15.1
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  // Automatically load required Grunt tasks
+  require('jit-grunt')(grunt, {
+    useminPrepare: 'grunt-usemin',
+    ngtemplates: 'grunt-angular-templates',
+    cdnify: 'grunt-google-cdn'
+  });
+
+  // Configurable paths for the application
+  var appConfig = {
+    root: './',
+    app: './app',
+    dist: './dist',
+    assets: './assets'
+  };
+
+  // Define the configuration for all the tasks
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    s: {
-      src: {
-        /*m: "static/min/",*/
-        js: "app/",
-        assets: "assets/"
-      },
-      prod: {
-        js: "prod/app/js/",
-        sass: "prod/app/css/",
-        assets: "prod/assets/",
-        prod: "prod/"
-      }
-    },
 
-    less: {
-      development: {
+    // Project settings
+    migree: appConfig,
+
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      bower: {
+        files: ['bower.json'],
+        tasks: ['wiredep']
+      },
+      js: {
+        files: ['<%= migree.app %>{,*/}*.js'],
+        tasks: ['newer:jshint:all', 'newer:jscs:all'],
         options: {
-          compress: true,
-          yuicompress: true,
-          optimization: 2
-        },
-        files: {
-          "assets/css/main.css": "assets/less/main.less"
+          livereload: '<%= connect.options.livereload %>'
         }
-      }
-    },
-
-    jshint: {
-      client: [ '<%= s.src.js %>'],
-      options: {
-          jshintrc: ".jshintrc"
-      }
-    },
-
-    autoprefixer: {
-      options: {
-        browsers: ['last 2 versions', 'ie 8', 'ie 9']
       },
-      main: {
-        src: 'assets/css/main.css'
+      jsTest: {
+        files: ['test/spec/{,*/}*.js'],
+        tasks: ['newer:jshint:test', 'newer:jscs:test', 'karma']
+      },
+      styles: {
+        files: ['<%= migree.assets %>/css/{,*/}*.css'],
+        tasks: ['newer:copy:styles', 'postcss']
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          'index.html',
+          '.tmp/styles/{,*/}*.css',
+          '<%= migree.assets %>/img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
       }
     },
 
-    concat: {
-      angular: {
-        src: ['bower_components/angular/angular.min.js',
-              'bower_components/angular-ui-router/release/angular-ui-router.min.js',
-              'bower_components/angular-route/angular-route.min.js',
-              'bower_components/angular-resource/angular-resource.min.js',
-              'bower_components/angular-q-spread/dist/q-spread.min.js'
-              ],
-        dest: 'static/vendor/angular-build.js',
-      }
-    },
-
-    clean: {
-      prod: ['prod']
-    },
-		copy: {
-      prod: {
-        expand: true, src: ['assets/**'], dest: 'prod/'
-      }
-		},
-		// clean + copy = prod / test
-    wiredep: {
-      target: {
-        src: 'index.html'
-      }
-    },
-
+    // The actual grunt server settings
     connect: {
       options: {
         port: 9000,
-        hostname: '0.0.0.0',//'migree.local',
-        open: 'http://migree.local:9000',
-        base: './',
-        livereload: 35729
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost',
+        livereload: 35729,
+        //hostname: '0.0.0.0',//'migree.local',
+        //open: 'http://migree.local:9000',
+        //base: './',
+
       },
       livereload: {
         options: {
           open: true,
           middleware: function (connect) {
-              return [
-                  modRewrite (['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.woff|\\.ttf$ /index.html [L]']),
-                  mountFolder(connect, './')
-              ];
+            return [
+              connect.static('.tmp'),
+              connect().use(
+                '/bower_components',
+                connect.static('./bower_components')
+              ),
+              connect().use(
+                '/assets/css',
+                connect.static('./assets/css')
+              ),
+              connect.static('./')
+            ];
+          }
+        }
+      },
+      test: {
+        options: {
+          port: 9001,
+          middleware: function (connect) {
+            return [
+              connect.static('.tmp'),
+              connect.static('test'),
+              connect().use(
+                '/bower_components',
+                connect.static('./bower_components')
+              ),
+              connect.static(appConfig.app)
+            ];
+          }
+        }
+      },
+      dist: {
+        options: {
+          open: true,
+          base: '<%= migree.dist %>'
+        }
+      }
+    },
+
+    // Make sure there are no obvious mistakes
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: {
+        src: [
+          'Gruntfile.js',
+          '<%= migree.app %>/{,*/}*.js'
+        ]
+      },
+      test: {
+        options: {
+          jshintrc: 'test/.jshintrc'
+        },
+        src: ['test/spec/{,*/}*.js']
+      }
+    },
+
+    // Make sure code styles are up to par
+    jscs: {
+      options: {
+        config: '.jscsrc',
+        verbose: true
+      },
+      all: {
+        src: [
+          'Gruntfile.js',
+          '<%= migree.app %>/{,*/}*.js'
+        ]
+      },
+      test: {
+        src: ['test/spec/{,*/}*.js']
+      }
+    },
+
+    // Empties folders to start fresh
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= migree.dist %>/{,*/}*',
+            '!<%= migree.dist %>/.git{,*/}*'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
+
+    // Add vendor prefixed styles
+    postcss: {
+      options: {
+        processors: [
+          require('autoprefixer-core')({browsers: ['last 1 version']})
+        ]
+      },
+      server: {
+        options: {
+          map: true
+        },
+        files: [{
+          expand: true,
+          cwd: '.tmp/css/',
+          src: '{,*/}*.css',
+          dest: '.tmp/css/'
+        }]
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '.tmp/styles/',
+          src: '{,*/}*.css',
+          dest: '.tmp/styles/'
+        }]
+      }
+    },
+
+    // Automatically inject Bower components into the app
+    wiredep: {
+      app: {
+        src: ['index.html'],
+        ignorePath:  /\.\.\//
+      },
+      test: {
+        devDependencies: true,
+        src: '<%= karma.unit.configFile %>',
+        ignorePath:  /\.\.\//,
+        fileTypes:{
+          js: {
+            block: /(([\s\t]*)\/{2}\s*?bower:\s*?(\S*))(\n|\r|.)*?(\/{2}\s*endbower)/gi,
+              detect: {
+                js: /'(.*\.js)'/gi
+              },
+              replace: {
+                js: '\'{{filePath}}\','
+              }
+            }
+          }
+      }
+    },
+
+    // Renames files for browser caching purposes
+    filerev: {
+      dist: {
+        src: [
+          '<%= migree.dist %>/scripts/{,*/}*.js',
+          '<%= migree.dist %>/css/{,*/}*.css',
+          '<%= migree.dist %>/img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          '<%= migree.dist %>/fonts/*'
+        ]
+      }
+    },
+
+    // Reads HTML for usemin blocks to enable smart builds that automatically
+    // concat, minify and revision files. Creates configurations in memory so
+    // additional tasks can operate on them
+    useminPrepare: {
+      html: 'index.html',
+      options: {
+        dest: '<%= migree.dist %>',
+        flow: {
+          html: {
+            steps: {
+              js: ['concat', 'uglifyjs'],
+              css: ['cssmin']
+            },
+            post: {}
           }
         }
       }
     },
 
-    watch: {
-      html: {
-        files: ['app/components/*.html'],
-        options: {
-          livereload: true,
-        }
-      },
-      less: {
-        files: ['assets/less/**/*'],
-        tasks: ['less'],
-        options: {
-          livereload: true,
-        }
-      },
-      js: {
-        files: ['<%= s.src.js %>**/*.js', 'gruntfile.js'],
-        tasks: ['jshint'],
-        options: {
-          spawn: false
-          //livereload: true,
+    // Performs rewrites based on filerev and the useminPrepare configuration
+    usemin: {
+      html: ['<%= migree.dist %>/{,*/}*.html'],
+      css: ['<%= migree.dist %>/css/{,*/}*.css'],
+      js: ['<%= migree.dist %>/scripts/{,*/}*.js'],
+      options: {
+        assetsDirs: [
+          '<%= migree.dist %>',
+          '<%= migree.dist %>/img',
+          '<%= migree.dist %>/css'
+        ],
+        patterns: {
+          js: [[/(images\/[^''""]*\.(png|jpg|jpeg|gif|webp|svg))/g, 'Replacing references to images']]
         }
       }
     },
 
-    uglify: {
-      scripts: {
-        files: {
-          '<%= s.src.m %>app.min.js': '<%= s.src.m %>app.js'
-        }
+    // The following *-min tasks will produce minified files in the dist folder
+    // By default, your `index.html`'s <!-- Usemin block --> will take care of
+    // minification. These next options are pre-configured if you do not wish
+    // to use the Usemin blocks.
+    // cssmin: {
+    //   dist: {
+    //     files: {
+    //       '<%= migree.dist %>/styles/main.css': [
+    //         '.tmp/styles/{,*/}*.css'
+    //       ]
+    //     }
+    //   }
+    // },
+    // uglify: {
+    //   dist: {
+    //     files: {
+    //       '<%= migree.dist %>/scripts/scripts.js': [
+    //         '<%= migree.dist %>/scripts/scripts.js'
+    //       ]
+    //     }
+    //   }
+    // },
+    // concat: {
+    //   dist: {}
+    // },
+
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= migree.app %>/images',
+          src: '{,*/}*.{png,jpg,jpeg,gif}',
+          dest: '<%= migree.dist %>/images'
+        }]
       }
     },
 
-    autoprefixer: {
-      files: ['assets/css/main.css'],
-      tasks: ['autoprefixer']
+    svgmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= migree.app %>/images',
+          src: '{,*/}*.svg',
+          dest: '<%= migree.dist %>/images'
+        }]
+      }
+    },
+
+    htmlmin: {
+      dist: {
+        options: {
+          collapseWhitespace: true,
+          conservativeCollapse: true,
+          collapseBooleanAttributes: true,
+          removeCommentsFromCDATA: true
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= migree.dist %>',
+          src: ['*.html'],
+          dest: '<%= migree.dist %>'
+        }]
+      }
+    },
+
+    ngtemplates: {
+      dist: {
+        options: {
+          module: 'testingappApp',
+          htmlmin: '<%= htmlmin.dist.options %>',
+          usemin: 'scripts/scripts.js'
+        },
+        cwd: '<%= migree.app %>',
+        src: 'views/{,*/}*.html',
+        dest: '.tmp/templateCache.js'
+      }
+    },
+
+    // ng-annotate tries to make the code safe for minification automatically
+    // by using the Angular long form for dependency injection.
+    ngAnnotate: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '.tmp/concat/scripts',
+          src: '*.js',
+          dest: '.tmp/concat/scripts'
+        }]
+      }
+    },
+
+    // Replace Google CDN references
+    cdnify: {
+      dist: {
+        html: ['<%= migree.dist %>/*.html']
+      }
+    },
+
+    // Copies remaining files to places other tasks can use
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= migree.app %>',
+          dest: '<%= migree.dist %>',
+          src: [
+            '*.{ico,png,txt}',
+            '*.html',
+            'images/{,*/}*.{webp}',
+            'fonts/{,*/}*.*'
+          ]
+        }, {
+          expand: true,
+          cwd: '.tmp/images',
+          dest: '<%= migree.dist %>/images',
+          src: ['generated/*']
+        }, {
+          expand: true,
+          cwd: 'bower_components/bootstrap/dist',
+          src: 'fonts/*',
+          dest: '<%= migree.dist %>'
+        }]
+      },
+      styles: {
+        expand: true,
+        cwd: '<%= migree.assets %>/css',
+        dest: '.tmp/css/',
+        src: '{,*/}*.css'
+      }
+    },
+
+    // Run some tasks in parallel to speed up the build process
+    concurrent: {
+      server: [
+        'copy:styles'
+      ],
+      test: [
+        'copy:styles'
+      ],
+      dist: [
+        'copy:styles',
+        /*'imagemin',
+        'svgmin'*/
+      ]
+    },
+
+    // Test settings
+    karma: {
+      unit: {
+        configFile: 'test/karma.conf.js',
+        singleRun: true
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-wiredep');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-autoprefixer');
 
-  // Default task(s).
-  grunt.registerTask('default', ['clean','less','wiredep', 'autoprefixer', 'concat', /*'uglify',*/ 'connect','watch']); // postcss??
+  grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'wiredep',
+      'concurrent:server',
+      'postcss:server',
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+
+  grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
+    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+    grunt.task.run(['serve:' + target]);
+  });
+
+  grunt.registerTask('test', [
+    'clean:server',
+    'wiredep',
+    'concurrent:test',
+    'postcss',
+    'connect:test',
+    'karma'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean:dist',
+    'wiredep',
+    'useminPrepare',
+    'concurrent:dist',
+    'postcss',
+    'ngtemplates',
+    'concat',
+    'ngAnnotate',
+    'copy:dist',
+    'cdnify',
+    'cssmin',
+    'uglify',
+    'filerev',
+    'usemin',
+    'htmlmin'
+  ]);
+
+  grunt.registerTask('default', [
+    'newer:jshint',
+    'newer:jscs',
+    'test',
+    'build'
+  ]);
 };
