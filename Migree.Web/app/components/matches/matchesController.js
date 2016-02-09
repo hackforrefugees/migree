4826281c0c06578e124bd6da798c7b6e9543efec
Johan Olsson <baktakt@gmail.com>
2016-02-09 20:37:05 +0100

    Fixed matches empty state

--- Migree.Web/app/components/matches/matchesController.js
+++ Migree.Web/app/components/matches/matchesController.js
@@ -1,22 +1,30 @@
 migree.controller('matchesController', ['$scope', 'matchesService', '$timeout', '$state',
   function ($scope, matchesService, $timeout, $state) {
 
+    $scope.gotoMessage = function() {
+      var userId = angular.element('.first').data('userId');
+      $state.go("message", { "user": userId});
+    };
+    $scope.flip = function ($event) {
+      var card = angular.element('.first').find('.card');
+      card.toggleClass('flipped');
+    };
+
     matchesService.matches.then(function (data) {
       $scope.matches = data;
-      $timeout(function () {
-        new ElastiStack(document.getElementById('stack'), {
-          distDragBack: 80,
-          distDragMax: 50,
-          onUpdateStack: function (current) { return false; }
-        });
-      }, 500);
-      $scope.gotoMessage = function() {
-        var userId = angular.element('.first').data('userId');
-        $state.go("message", { "user": userId});
-      };
-      $scope.flip = function ($event) {
-        var card = angular.element('.first').find('.card');
-        card.toggleClass('flipped');
-      };
+      if(data.length>0) {
+        $timeout(function () {
+          angular.element('.cardbuttons').removeClass('hidden');
+          angular.element('.elasticstack').removeClass('hidden');
+          new ElastiStack(document.getElementById('stack'), {
+            distDragBack: 80,
+            distDragMax: 50,
+            onUpdateStack: function (current) { return false; }
+          });
+        }, 500);
+      }
+      else {
+        angular.element('.emptystate').removeClass('hidden');
+      }
     });
   }]);
