573bbb884da0b12466b709e285c7ca4ab45b0c60
Andreas Urech <andreas.urech@gmail.com>
2016-02-09 17:22:54 +0100

    Message refactor

--- Migree.Web/app/components/message/messageController.js
+++ Migree.Web/app/components/message/messageController.js
@@ -1,33 +1,40 @@
-migree.controller('messageController', ['$scope', '$stateParams', 'apiService', function ($scope, $stateParams, apiService) {
+migree.controller('messageController', ['$scope', '$stateParams', 'messageService', function ($scope, $stateParams, messageService) {
 
   var self = this;
   self.toUserId = $stateParams.user;
 
   $scope.message = null;
 
-  self.thread = apiService.messageThread.query({userId: self.toUserId});
-  $scope.sendButtonText = 'Start conversation';
+  messageService.getThread({userId: self.toUserId})
+    .then(function(thread) { self.thread = thread; })
+    .then(function() {
 
-  /* Handle this later on :) */
-  if(self.thread.length) {
-    if(self.thread[0].isUser) {
-      $scope.sendButtonText = 'Send message';
-    }
-    else {
-      $scope.sendButtonText = 'Reply';
-    }
-  }
+      $scope.sendButtonText = 'Start conversation';
 
-  $scope.thread = self.thread;
+      /* Handle this later on :) */
+      if(self.thread.length) {
+        if(self.thread[0].isUser) {
+          $scope.sendButtonText = 'Send message';
+        }
+        else {
+          $scope.sendButtonText = 'Reply';
+        }
+      }
+
+      $scope.thread = self.thread;
+    });
 
   $scope.sendMessage = function() {
     if(!$scope.editable) {
-      apiService.message.save({userId: self.toUserId, message: $scope.message}, function(data) {
+      messageService.saveMessage({userId: self.toUserId, message: $scope.message}).then(function(data) {
         self.thread.unshift({
           isUser: true,
           content: $scope.message,
           created: 'a moment ago'
         });
+
+        $scope.message = null;
+
       });
     }
   };
--- Migree.Web/app/components/messages/messagesController.js
+++ Migree.Web/app/components/messages/messagesController.js
@@ -1,3 +1,8 @@
-migree.controller('messagesController', ['$scope', 'apiService', function ($scope, apiService) {
-  $scope.messages = apiService.message.query();
+migree.controller('messagesController', ['$scope', 'messageService', function ($scope, messageService) {
+  messageService.getMessage().then(function(data) {
+    //$scope.messages = data
+    console.log(data);
+  });
+
+
 }]);
