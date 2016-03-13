86497c0f583bfb41bbb812624abb585eb4cb8c80
Johan <baktakt@gmail.com>
2016-03-13 21:46:22 +0100

    Started settings view

--- Migree.Web/app/components/settings/settingsController.js
+++ Migree.Web/app/components/settings/settingsController.js
@@ -1,10 +1,23 @@
-﻿migree.controller('settingsController', ['$scope', 'settingsService',
-  function ($scope, settingsService) {
+﻿migree.controller('settingsController', ['$scope', 'settingsService', '$q',
+  function ($scope, settingsService, $q) {
     
     settingsService.user.query().$promise.then(function (data) {
       console.log(data);
       $scope.settings = data;      
     });
+
+    var promises = [
+      settingsService.competencePromise,
+      settingsService.businessPromise,
+      settingsService.locationPromise
+    ];
+
+    $q.all(promises).spread(function (competences, businesses, locations) {
+      $scope.competences = competences;
+      $scope.businesses = businesses;
+      $scope.locations = locations;
+    });
+
 
     $scope.update = function () {
       
