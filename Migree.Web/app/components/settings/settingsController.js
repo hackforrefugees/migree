1c908887d6352a0454779612ebcf95ca11809ad1
Johan <baktakt@gmail.com>
2016-03-15 21:42:08 +0100

    Fixed selected competences

--- Migree.Web/app/components/settings/settingsController.js
+++ Migree.Web/app/components/settings/settingsController.js
@@ -17,15 +17,25 @@
         $scope.locations.selected = $scope.locations.filter(function(location) {
             return location.id === $scope.settings.userLocation;
           })[0];
+        $scope.settings.competences.selected = getFilteredArray($scope.competences, $scope.settings.competences);
       });
 
     });
-
-    
-
     
 
     $scope.update = function () {
       
     };
+
+    function getFilteredArray(inputArray, filter) {
+        var filteredArray = [];
+        for (var i=0;i<inputArray.length; i++) {
+            for(var p=0;p<filter.length;p++) {
+              if(filter[p] === inputArray[i].id) {
+                filteredArray.push(inputArray[i]);
+              }
+            }
+        }
+        return filteredArray;
+    }
 }]);
