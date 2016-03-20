a6de2e46b28a7a810d2e74e81484f33cbf3db99c
Johan <baktakt@gmail.com>
2016-03-20 22:32:08 +0100

    Fixed matches css prefixes

--- Migree.Web/app/components/messages/messagesController.js
+++ Migree.Web/app/components/messages/messagesController.js
@@ -2,6 +2,9 @@ migree.controller('messagesController', ['$scope', 'messageService', '$state',
   function ($scope, messageService, $state) {
     messageService.getMessages().then(function (data) {
       $scope.messages = data;
+      if(data.length<=0) {
+      	angular.element('.emptystate').removeClass('hidden');
+      }
     });
 
     $scope.gotoMessage = function (userId) {
