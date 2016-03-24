680a37452e7fcb5820c95f25a8adb0d1ad38e740
Andreas Urech <andreas.urech@gmail.com>
2016-03-24 23:09:58 +0100

    bugfix, user avatar handling

--- Migree.Web/app/components/settings/settingsService.js
+++ Migree.Web/app/components/settings/settingsService.js
@@ -1,11 +1,21 @@
-﻿migree.factory('settingsService', ['apiService', function (apiService) {
+﻿migree.factory('settingsService', ['apiService', '$q', function (apiService, $q) {
 
   var imageUpload = function (file) {
+    var deferred = $q.defer();
     if(file) {
       var formData = new FormData();
-      formData.append('Content', file);
-      apiService.imageUpload(formData);  
+      formData.append('Content', file, "scaled.png");
+      apiService.imageUpload(formData).then(function(success) {
+        deferred.resolve(success);
+      }, function(fail) {
+        deferred.reject(fail);
+      });
     }
+    else {
+      deferred.reject({'status': 'No image provided'});
+    }
+
+    return deferred.promise;
   };
 
   return {
