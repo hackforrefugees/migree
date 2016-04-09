35ab6be907009dd530516834e51fca8aba504b9a
Andreas Urech <andreas.urech@gmail.com>
2016-04-09 07:48:53 +0200

    Bugfix. Promises added to service to handle success/failure of image upload.

--- Migree.Web/app/components/settings/settingsService.js
+++ Migree.Web/app/components/settings/settingsService.js
@@ -5,19 +5,21 @@
     if(file) {
       var formData = new FormData();
       formData.append('Content', file);
-      apiService.imageUpload(formData);
 
-      deferred.resolve();
+      apiService.imageUpload(formData).then(function(data) {
+        deferred.resolve();
+      }, function(fail) {
+        deferred.reject();
+      });
     }
     else {
       deferred.reject({'status': 'No image provided'});
     }
-
     return deferred.promise;
   };
 
   return {
-    user: apiService.user,    
+    user: apiService.user,
     competencePromise: apiService.competence.query().$promise,
     locationPromise: apiService.location.query().$promise,
     imageUpload: imageUpload
